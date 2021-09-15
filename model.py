a505770713f54f9758c624a715c0f12b1fc57871
kyselica12 <36887144+kyselica12@users.noreply.github.com>
2021-09-15 10:04:46 +0200

    dummy model for testing

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 from game import GameState
+import numpy as np
 
 
 class Model(ABC):
@@ -9,3 +10,13 @@ class Model(ABC):
     def predict(self, state: GameState):
         pass
 
+class DummyModel(Model):
+
+    def __init__(self, size=15):
+        self.size = size
+
+    def predict(self, state: GameState):
+        reward = 0
+        probs = np.ones((self.size, self.size)) * 1 / (self.size ** 2)
+
+        return reward, probs
