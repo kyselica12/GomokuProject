7cadbd6b25d8b49998d71fdce61b866fd1b1eb7a
kyselica12 <36887144+kyselica12@users.noreply.github.com>
2021-09-21 09:38:27 +0200

    self_play training

--- main.py
+++ main.py
@@ -1,9 +1,68 @@
+import time
+
+import tqdm
+
 from game import Game
 from nn.betazero import BetaZeroConfig
+from nn.dataset import GameDatabase
 from nn.nn_wraper import NetWrapper
 from play.human_player import HumanPlayer
 from play.mcts_player import MCTSPlayer
-from play.play import play_game
+from play.play import play_game, play_games
+
+
+from copy import deepcopy
+
+class SelfPlayTraining:
+    MODEL_PATH = "./"
+
+    def __init__(self, model_path, model_conf: BetaZeroConfig, mcts_iters, storage_size):
+        self.size = model_conf.board_size
+        self.mcts_iters = mcts_iters
+        self.storage_size = storage_size
+        self.database = GameDatabase(storage_size, board_size=self.size)
+
+        self.best_model = NetWrapper(model_conf)
+        self.best_model.load_model(model_path)
+        self.training_model = deepcopy(self.best_model)
+
+        self.game = Game(self.size, win_size=5)
+
+
+
+    def fill_database(self):
+
+        p1 = MCTSPlayer(self.game, self.best_model, exploration=True, iters=self.mcts_iters)
+        p2 = MCTSPlayer(self.game, self.best_model, exploration=True, iters=self.mcts_iters)
+
+        states, _ = play_games(p1, p2, self.game, n_games=self.storage_size, random_start=True)
+        self.database.add_games(states)
+
+        if not self.database.full():
+
+            while not self.database.full():
+                state = play_game(p1, p2, self.game, show=False, random_start=True)
+                self.database.add_games([state])
+
+    def _next_population(self):
+        self.best_model = self.training_model
+        self.best_model.save_model(folder=self.MODEL_PATH, name=f"best_model_{time.time_ns()}")
+        self.training_model = deepcopy(self.best_model)
+
+    def self_play(self, n_games=20, n_iters=50, batch_size=5):
+
+        p1 = MCTSPlayer(self.game, self.best_model, exploration=True, iters=self.mcts_iters)
+        p2 = MCTSPlayer(self.game, self.training_model, exploration=True, iters=self.mcts_iters)
+
+        states, score = play_games(p1, p2, self.game, n_games, random_start=True)
+        print(f"Score: {score}")
+        self.database.add_games(states)
+
+        if score['p2'] / n_games > 0.55:
+            self._next_population()
+        else:
+            self.training_model.self_play_train(self.database, batch_size, n_iters, loss_visual_step=int(n_iters//10))
+
 
 if __name__ == "__main__":
     conf = BetaZeroConfig(board_size=15,
@@ -22,3 +81,5 @@ if __name__ == "__main__":
     player2 = MCTSPlayer(game, wrapper, exploration=False, iters=100)
 
     state = play_game(player1, player2, game, show=True)
+
+
--- play/play.py
+++ play/play.py
@@ -1,11 +1,22 @@
+import random
+
+import tqdm
 
 from game import Game, GameState
 from play.player import Player
 
 
-def play_game(player1: Player, player2: Player, game: Game, show=False):
+def play_game(player1: Player, player2: Player, game: Game, show=False, random_start=False):
 
     state = game.get_new_state()
+
+    if random_start:
+        move = (random.randrange(game.size), random.randrange(game.size))
+        state = game.move(state, move)
+
+        available_moves = game.available_moves(state)
+        move = available_moves[random.randrange(len(available_moves))]
+        state = game.move(state, move)
 
     if show:
         print(state)
@@ -24,15 +35,15 @@ def play_game(player1: Player, player2: Player, game: Game, show=False):
     return state
 
 
-def play_games(player1, player2, game, n_games):
+def play_games(player1, player2, game, n_games, random_start=False):
 
     p1, p2 = player1, player2
     p1_name, p2_name = 'p1', 'p2'
 
     final_states = []
     score = {p1_name:0, p2_name:0, 'draw':0}
-    for i in range(n_games):
-        state = play_game(p1, p2, game, show=False)
+    for i in tqdm.tqdm(range(n_games), desc="Playing Games "):
+        state = play_game(p1, p2, game, show=False, random_start=random_start)
         final_states.append(state)
 
         if state.reward == 0:
@@ -46,5 +57,7 @@ def play_games(player1, player2, game, n_games):
 
         p1, p2 = p2, p1
         p1_name, p2_name = p2_name, p1_name
+
+    return final_states, score
 
 
